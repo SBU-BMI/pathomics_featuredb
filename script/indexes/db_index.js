12305e55973662c663d2151dc8e7d43c51888bd2
tkurc <tkurc@172.21.77.59>
2016-05-27 07:09:34 -0400

    Added indexes on analysis_execution_id

--- script/indexes/db_index.js
+++ script/indexes/db_index.js
@@ -3,6 +3,7 @@ var coll = db.objects;
 coll.createIndex( {'provenance.image.case_id' : 1, 'provenance.analysis.execution_id' : 1, 'footprint' : 1, 'x' : 1, 'y': 1}, {'name' : "exe_case_fp_xy"});
 coll.createIndex( {'provenance.image.case_id' : 1, 'provenance.analysis.execution_id' : 1, 'randval': 1}, {'name' : "case_exe_randval"});
 coll.createIndex( {'provenance.image.subject_id' : 1, 'provenance.analysis.execution_id' : 1, 'randval': 1}, {'name' : "sub_exe_randval"});
+coll.createIndex( {'provenance.analysis.execution_id' : 1, 'randval': 1}, {'name' : "exe_randval"});
 coll.createIndex( {'randval' : 1} );
 var collimg = db.images;
 collimg.createIndex( {'subject_id' : 1, 'caseid_id' : 1}, {unique: true});
