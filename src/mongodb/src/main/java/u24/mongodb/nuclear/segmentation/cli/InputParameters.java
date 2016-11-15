13d5deeb728ddbdc410bf28c60406ac569c1c414
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2016-11-15 14:05:50 -0500

    variable listing
    InputParameters and CommandLineArguments use the same variables.  Keep them in the same order so you can more easily see what's going on.

--- src/mongodb/src/main/java/u24/mongodb/nuclear/segmentation/cli/InputParameters.java
+++ src/mongodb/src/main/java/u24/mongodb/nuclear/segmentation/cli/InputParameters.java
@@ -2,38 +2,46 @@ package u24.mongodb.nuclear.segmentation.cli;
 
 import java.io.FileWriter;
 
-public class InputParameters {	
-	    public String dbServer = null;
-	    public String inpType = null;
-	    public String inputList = null;
-	    public String outFolder = null;
-	    public String caseID = null;
-	    public String subjectID = null;
-	    public boolean isQuip = false;
-	    public boolean doNormalize = false;
-	    public boolean selfNormalize = false;
-	    public boolean doSimplify = false;
-	    public double minSize = 0.0;
-	    public double maxSize = 40000000000.0;
-	    public String execID = null;
-	    public String studyID = null;
-		public String batchID = null;
-		public String tagID = null;
-	    public String execType = null;
-	    public String execTitle = null;
-	    public String colorVal = null;
-	    public String execComp = null;
-	    public String nameSpace = null;
-	    public boolean getFromDB = false;
-	    public int shiftX = 0;
-	    public int shiftY = 0;
-	    public int width = 1;
-	    public int height = 1;
-	    public String imageFile = null;
-	    public String inputFile = null;
-	    
-	    public FileWriter outFileWriter = null;
-	    
-	    public InputParameters() { }
-	    
+public class InputParameters {
+    public String dbServer = null;
+    
+    public String inpType = null;
+    public boolean isQuip = false;
+    
+    public String inputList = null;
+    public String imageFile = null;
+    public String inputFile = null;
+    public String outFolder = null;
+    
+    public String caseID = null;
+    public String subjectID = null;
+    
+    public boolean doNormalize = false;
+    public boolean selfNormalize = false;
+    
+    public boolean getFromDB = false;
+    
+    public int width = 1;
+    public int height = 1;
+    public int shiftX = 0;
+    public int shiftY = 0;
+    
+    public String execID = null;
+    public String studyID = null;
+    public String batchID = null;
+    public String tagID = null;
+    public String execType = null;
+    public String execTitle = null;
+    public String colorVal = null;
+    public String execComp = null;
+    public String nameSpace = null;
+    
+    public boolean doSimplify = false;
+    public double minSize = 0.0;
+    public double maxSize = 40000000000.0;
+    
+    public FileWriter outFileWriter = null;
+    
+    public InputParameters() { }
+    
 }
